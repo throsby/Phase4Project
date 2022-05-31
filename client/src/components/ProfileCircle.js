9e8e7b44b502f6282783008cffc979af321fbce3
Throsby Wells <throsbywells@gmail.com>
2022-05-31 16:36:22 -0400

    adjusted stuff in css

--- client/src/components/ProfileCircle.js
+++ client/src/components/ProfileCircle.js
@@ -1,7 +1,9 @@
 const ProfileCircle = ({potato}) => {
     return(
         <div className="profile-circle">
+            <div>
             <p>{potato}</p>
+            </div>
         </div>
     )
 }
