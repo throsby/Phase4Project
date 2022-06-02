96f8f8013e33714dade5e0040077439fedfe6806
Throsby Wells <throsbywells@gmail.com>
2022-06-02 10:01:28 -0400

    fixed initial 403 error

--- client/src/components/LoginLink.js
+++ client/src/components/LoginLink.js
@@ -4,7 +4,7 @@ import Signup from "./Signup"
 
 const LoginLink = () => {
 
-    const [ user, setUser ] = useState(null)
+    const [ , setUser ] = useState(null)
     const [ loginLink, setLoginLink ] = useState(false)
     const [ signUp, setSignUp ] = useState(false)
     const [ username, setUsername ] = useState("")
@@ -19,11 +19,12 @@ const LoginLink = () => {
     }
 
     useEffect(()=> {
+        if (localStorage.length > 0){
         (async ()=> {
             let req = await fetch(`http://localhost:3000/me/${localStorage.getItem("session_user_id")}`)
             let res = await req.json()
             setUsername(res.username)
-        })()
+        })()}
     },[])
 
     const handleLogout = async () => {
@@ -42,7 +43,7 @@ const LoginLink = () => {
             {username ? <div>Welcome <span style={{cursor:'pointer', textDecoration: "underline"}}>{username}</span> <span style={{cursor:'pointer', textDecoration: "underline"}} onClick={handleLogout}>Logout</span></div>
                 :
                     <div>Welcome, Guest!
-                            <span style={{cursor:'pointer', textDecoration: "underline"}} onClick={handleClick}>Login here!</span>  <br/>
+                            <span style={{cursor:'pointer', textDecoration: "underline"}} onClick={handleClick}>Login here!</span> <br/>
                             <span style={{cursor:'pointer', textDecoration: "underline"}} >New? Sign up</span>
                     </div>}
             {loginLink && <LoginForm setSignUp={setSignUp} setLoginLink={setLoginLink} setUsername={setUsername}/>}
