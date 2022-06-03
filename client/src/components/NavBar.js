f80e24d51588fe239c17262b5b21e385222b6a69
Justin Chazen <Justinchazen96@gmail.com>
2022-06-03 09:44:13 -0400

    made navlinks

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,5 +1,6 @@
 import LoginLink from "./LoginLink"
 import { useState,useEffect } from "react";
+import { NavLink } from 'react-router-dom'
 
 
 const NavBar = ({username, setUsername}) => {
@@ -20,6 +21,18 @@ const NavBar = ({username, setUsername}) => {
     return(
         <div className="navbar">
             <LoginLink username={username} setUsername={setUsername}/>
+                <div className='home-nav'>
+                    <NavLink to='/' exact>HomePage</NavLink>
+                </div>
+                <div className='profile-nav'>
+                    <NavLink to='/profile-page' exact>Profile</NavLink>
+                </div>
+                <div className='store-nav'>
+                    <NavLink to='/store' exact>Store</NavLink>
+                </div>
+                <div className='cart-nav'>
+                    <NavLink to='/cart' exact>Cart</NavLink>
+                </div>
         </div>
     )
 }
